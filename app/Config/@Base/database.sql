b603f988a52076faffb9582ca3081ddf5e4f2f2b
Pablo Henrick Diniz <phydokz@hotmail.com>
2015-02-01 12:51:45 -0300

    #1 completo

--- app/Config/@Base/database.sql
+++ app/Config/@Base/database.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: 29-Jan-2015 às 02:23
+-- Generation Time: 01-Fev-2015 às 16:51
 -- Versão do servidor: 5.6.16
 -- PHP Version: 5.5.9
 
@@ -24,7 +24,7 @@ CREATE TABLE IF NOT EXISTS `config` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `last_project_id` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`),
-  KEY `last_project_id` (`last_project_id`)
+  KEY `config_ibfk_1` (`last_project_id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=2 ;
 
 -- --------------------------------------------------------
@@ -35,18 +35,19 @@ CREATE TABLE IF NOT EXISTS `config` (
 
 CREATE TABLE IF NOT EXISTS `map` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `project_id` int(11) NOT NULL,
+  `project_id` int(11) DEFAULT NULL,
   `parent_id` int(11) DEFAULT NULL,
   `name` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `display_name` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `width` int(11) NOT NULL DEFAULT '5',
-  `height` int(11) NOT NULL DEFAULT '5',
-  `scroll_type` int(11) DEFAULT '0',
+  `display` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `width` int(11) NOT NULL DEFAULT '10',
+  `height` int(11) NOT NULL DEFAULT '10',
+  `scroll` int(11) DEFAULT '0',
   `bgm` int(11) DEFAULT NULL,
+  `expand` tinyint(1) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`),
   KEY `map_ibfk_1` (`project_id`),
   KEY `map_ibfk_2` (`parent_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=52 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=176 ;
 
 -- --------------------------------------------------------
 
@@ -57,8 +58,9 @@ CREATE TABLE IF NOT EXISTS `map` (
 CREATE TABLE IF NOT EXISTS `project` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(512) COLLATE utf8_unicode_ci NOT NULL,
+  `expand` tinyint(1) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=75 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=90 ;
 
 -- --------------------------------------------------------
 
@@ -84,11 +86,11 @@ CREATE TABLE IF NOT EXISTS `users` (
 -- Limitadores para a tabela `config`
 --
 ALTER TABLE `config`
-ADD CONSTRAINT `config_ibfk_1` FOREIGN KEY (`last_project_id`) REFERENCES `project` (`id`);
+ADD CONSTRAINT `config_ibfk_1` FOREIGN KEY (`last_project_id`) REFERENCES `project` (`id`) ON DELETE SET NULL ON UPDATE SET NULL;
 
 --
 -- Limitadores para a tabela `map`
 --
 ALTER TABLE `map`
-ADD CONSTRAINT `map_ibfk_2` FOREIGN KEY (`parent_id`) REFERENCES `map` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-ADD CONSTRAINT `map_ibfk_1` FOREIGN KEY (`project_id`) REFERENCES `project` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+ADD CONSTRAINT `map_ibfk_1` FOREIGN KEY (`project_id`) REFERENCES `project` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+ADD CONSTRAINT `map_ibfk_2` FOREIGN KEY (`parent_id`) REFERENCES `map` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
